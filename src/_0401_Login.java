6c2aa37b8f1090d469c1f9f83a07e508cad40598
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-03-13 16:02:51 +0100

    Test Case 401 is completed.

---
+++ src/_0401_Login.java
@@ -0,0 +1,35 @@
+import Utility.BaseDriver;
+import Utility.MyFunctions;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
+public class _0401_Login extends BaseDriver {
+    @Test
+    public void test0301(){
+
+        driver.get("https://demowebshop.tricentis.com/");
+
+        WebElement loginLink = driver.findElement(By.linkText("Log in"));
+        loginLink.click();
+
+        WebElement emailBox = driver.findElement(By.id("Email"));
+        emailBox.sendKeys(MyFunctions.getEmail());
+
+        WebElement passwordBox = driver.findElement(By.id("Password"));
+        passwordBox.sendKeys(MyFunctions.getPassword());
+
+        WebElement submit = driver.findElement(By.cssSelector("input[class='button-1 login-button']"));
+        submit.click();
+
+        WebElement username = driver.findElement(By.cssSelector("a[class='account']"));
+
+        Assert.assertTrue("username is false",
+                username.getText().equals(MyFunctions.getEmail()));
+
+        WaitClose();
+    }
+
+}
